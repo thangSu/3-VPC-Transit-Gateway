f3e6e2813ebf633c00861de5eadda41c44f729f4
quangnghi99 <quangnghi2402@gmail.com>
2023-01-30 00:40:55 +0700

    update

--- modules/vpc/vpc-2.tf
+++ modules/vpc/vpc-2.tf
@@ -59,16 +59,11 @@ resource "aws_nat_gateway" "nat_gw" {
 resource "aws_route_table" "vpc_2_private_rt" {
   vpc_id = aws_vpc.vpc_2.id
 
-  # transit gateway
-  route {
-    cidr_block = "10.0.0.0/8"
-    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  }
-
-  # Nat gateway
+  # Nat gateway and transit gateway
   route {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
+    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   }
 
   tags = {
